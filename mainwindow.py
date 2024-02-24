238f81534484a079934dd5eace6f81b81a75d5c3
Nik <example@gmail.com>
2024-02-24 15:15:39 +0300

    Add labels for cpu cores

--- mainwindow.py
+++ mainwindow.py
@@ -1,39 +1,56 @@
 import tkinter as tk
 from tkinter import ttk
 import sys
+from process import Process
 
 class App(tk.Tk):
 
     def __init__(self):
         tk.Tk.__init__(self)
-        self.attributes('-alpha', 0.5)
+        self.attributes('-alpha', 0.8)
         self.attributes('-topmost', True)
         self.overrideredirect(True)
         self.resizable(False, False)
         self.title('CPU-RAM')
 
+        self.cpu = Process()
         self.set_ui()
+        self.Cpu_usage_bar()
 
     def set_ui(self):
         exit_button = ttk.Button(self, text='Exit', command=self.app_exit)
         exit_button.pack(fill=tk.X)
 
-        self.frame2 = ttk.LabelFrame(self, text='Manual')
-        self.frame2.pack(fill=tk.X)
+        self.frame1 = ttk.LabelFrame(self, text='Manual')
+        self.frame1.pack(fill=tk.X)
 
-        self.combobox = ttk.Combobox(self.frame2, values=['hiden', 'full', 'minimum'],width=10, state='readonly')
+        self.combobox = ttk.Combobox(self.frame1, values=['hiden', 'full', 'minimum'],width=10, state='readonly')
         self.combobox.current(1)
         self.combobox.pack(side=tk.LEFT)
 
 
-        ttk.Button(self.frame2, text='move').pack(side=tk.LEFT)
-        ttk.Button(self.frame2, text='>>>').pack(side=tk.LEFT)
+        ttk.Button(self.frame1, text='move').pack(side=tk.LEFT)
+        ttk.Button(self.frame1, text='>>>').pack(side=tk.LEFT)
 
         self.frame2 = ttk.LabelFrame(self, text='Power')
         self.frame2.pack(fill=tk.BOTH)
 
         self.bind_class('Tk', '<Enter>', self.enter_mouse)
         self.bind_class('Tk', '<Leave>', self.leave_mouse)
+
+    def Cpu_usage_bar(self):
+        ttk.Label(self.frame2, text=f'Physical cores: {self.cpu.cpu_count}, logical cores: {self.cpu.cpu_count_logical}',
+                    anchor=tk.CENTER).pack(fill=tk.X)
+        
+        self.list_label = []
+        self.list_progressbar = []
+
+        for i in range(self.cpu.cpu_count_logical):
+            self.list_label.append(ttk.Label(self.frame2, anchor=tk.CENTER))
+            self.list_progressbar.append(ttk.Progressbar(self.frame2, length=100))
+        for i in range(self.cpu.cpu_count_logical):
+            self.list_label[i].pack(fill=tk.X)
+            self.list_progressbar[i].pack(fill=tk.X)
 
 
     def enter_mouse(self, event):
@@ -47,5 +64,7 @@ class App(tk.Tk):
     def app_exit(self):
         self.destroy()
         sys.exit()
-root = App()
-root.mainloop()
+
+if __name__ == '__main__':
+    root = App()
+    root.mainloop()
